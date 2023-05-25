8f326ab236a556b159e611d82d601c160dff11f0
Amna Al Hammadi <106449286+amnaalhammadi@users.noreply.github.com>
2023-05-25 12:14:43 +0400

    Add files via upload

---
+++ rush00/rush00.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Amna - <aal-hamm@student42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/22 11:05:37 by plucena-          #+#    #+#             */
+/*   Updated: 2022/05/22 16:30:04 by plucena-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	rush(int x, int y)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i <= y)
+	{
+		j = 1;
+		while (j <= x)
+		{
+			if ((i == 1 && j == 1) || (i == y && j == x))
+				ft_putchar('o');
+			else if ((i == 1 && j == x) || (i == y && j == 1))
+				ft_putchar('o');
+			else if (i == 1 || i == y)
+				ft_putchar('-');
+			else if (j == 1 || j == x)
+				ft_putchar('|');
+			else
+				ft_putchar(' ');
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+}
